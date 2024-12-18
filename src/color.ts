4cfaa855804b8c6d020a9c8967a649918dd46a87
Ese-Tech <eseosagie22@gmail.com>
2024-12-18 23:33:49 +0100

    Enhance type safety by typecasting elements in color.ts

--- src/color.ts
+++ src/color.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+// Get elements by their IDs and typecast them for better type safety
+const changeColorButton = document.getElementById(
+	"changeColorButton"
+) as HTMLButtonElement;
+const resetColorButton = document.getElementById(
+	"resetColorButton"
+) as HTMLButtonElement;
+const colorCodeElement = document.getElementById("colorCode") as HTMLDivElement;
+const colorHistoryElement = document.getElementById(
+	"colorHistory"
+) as HTMLDivElement;
