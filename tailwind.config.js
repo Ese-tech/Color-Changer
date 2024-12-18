c052d9250e6f9d6fb3a2ac73cc774c3c0a41c185
Ese-Tech <eseosagie22@gmail.com>
2024-12-19 00:03:10 +0100

    Refactor color.html structure and styles; remove old file and add new version with Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 export default {
 	content: [
 		"./src/**/*.{html,js,ts,tsx}", // Watch all your HTML/JS/TS files in the src folder
+		"./*.html", // Add this line to include HTML files in the root directory
 	],
 	theme: {
 		extend: {},
