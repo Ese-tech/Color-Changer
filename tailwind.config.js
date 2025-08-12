ac2b3ed056c26765db4d2469fb28d3b4f601f36d
Ese-tech <ese.osagie19@gmail.com>
2025-08-12 16:31:37 +0200

    Update output path for CSS build and enhance Tailwind CSS configuration to include public directory files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: [
-		"./src/**/*.{html,js,ts,tsx}", // Watch all your HTML/JS/TS files in the src folder
-		"./*.html", // Add this line to include HTML files in the root directory
-	],
+  "./src/**/*.{html,js,ts,tsx}",
+  "./*.html",
+  "./public/**/*.html",
+  "./public/**/*.js"
+],
 	theme: {
 		extend: {},
 	},
